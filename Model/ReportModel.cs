9f9c4383d580249ee4ad48703d61a6e1a0d17d1e
leafcxy <cxy1337@gmail.com>
2024-11-27 14:33:52 +0800

    Add project files.

---
+++ Model/ReportModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace HtmlReport.Model
+{
+    public class ReportModel
+    {
+
+        public Dictionary<string, object> Parameters = new Dictionary<string, object>();
+        public Dictionary<string, Font> Fonts = new Dictionary<string, Font>();
+        public object DataSource { get; set; }
+        public object DetailReport { get; set; }
+        public object DetailReport2 { get; set; }
+
+
+        public int PageHeight { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReportProcess.Instance.PrintReport(report, reportModel, item.Key.PrinterDeviceName);
+        }
+    }
+}
